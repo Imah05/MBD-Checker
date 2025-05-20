37e0d2cec4f67729491ba6d37b31a0872a52a540
Imah05 <hametnerpaul05@gmail.com>
2025-05-20 14:20:56 +0200

    proofs for all sur 7 lemmas except for Lemma 13 (14 vtx in R and no deg 1 vtx)

--- RSI.cpp
+++ RSI.cpp
@@ -5,32 +5,72 @@ using namespace std;
 bool check_all_RSI_Lemmas() {
     // use nauty-genbg and nauty-geng on the institute computers!!!
 
-    // checks, whether D. wins on every bipartite graph with bipartition (S,I) 
+
+
+    // SUR 7
+
+    // checks, that D. wins (going second) on every graph with at least 
+    // 4 and at most 6 vertices, minimum degree 1 and sum_{v != w} |deg(v)-3| <= 4, 
+    // where w is a maxdeg vtx, except for two exceptions (one with 
+    // 5 vertices and one with 6 vertices)
+    if(!check_R("geng -d1 4", 'S', 4).empty())
+        return false;
+    if(check_R("geng -d1 5", 'S', 4).size() != 1)
+        return false;
+    if(check_R("geng -d1 6", 'S', 4).size() != 1)
+        return false;
+
+    // checks, whether D. wins (going first) on every bipartite graph with bipartition (S,I) 
     // such that |S| = 8, |I| = 9, deg(i) = 3 for i in I, 2 <= deg(s) <= 7 
-    // for all s in S and at most 4 degree 2 vtx going first.
+    // for all s in S and at most 4 degree 2 vtx.
     if(!check_SI("genbg -d2:3 -D7:3 8 9", 'D', 4))
         return false;
 
-    // checks, whether D. wins on every bipartite graph with bipartition (S,I) 
+    // checks, whether D. wins (going first) on every bipartite graph with bipartition (S,I) 
     // such that |S| = 7, |I| = 8, deg(i) = 3 for i in I, 2 <= deg(s) 
-    // for all s in S and at most 4 degree 2 vtx going first.
+    // for all s in S and at most 4 degree 2 vtx.
     if (!check_SI("genbg -d2:3 -D8:3 7 8", 'D', 4))
         return false;
 
-    // checks, whether D. wins on every bipartite graph with bipartition (S,I) 
+    // checks, whether D. wins (going first) on every bipartite graph with bipartition (S,I) 
     // such that |S| = 7, |I| = 9, 2 <= deg(i) <= 3 for all i in I, 2 <= deg(s) 
-    // for all s in S and at most 3 degree 2 vtx going first.
+    // for all s in S and at most 3 degree 2 vtx.
     if (!check_SI("genbg -d2:2 -D9:3 7 9", 'D', 3))
         return false;
 
-    // checks, whether D. wins on every bipartite graph with bipartition (S,I) 
+    // checks, whether D. wins (going second) on every bipartite graph with bipartition (S,I) 
     // such that 3 <= |S| <= 6 , |I| = |S|+1, deg(i) = 3 for all i in I, 2 <= deg(s) 
-    // for all s in S and at most 4 degree 2 vtx going second.
+    // for all s in S and at most 4 degree 2 vtx.
     for (int s = 6; s > 2; --s) {
         string cmd = "genbg -d2:3 -D" + to_string(s+1) + ":3 " + to_string(s) + " " + to_string(s+1);
         if (!check_SI(cmd, 'S', 4))
             return false;
     }
+
+
+    // checks, whether D. wins (going first) on every graph on at most 12 
+    // vertices, minimum degree 1 and sum_{v != w} |deg(v)-3| <= 4, 
+    // where w is a maxdeg vtx.
+    if(!check_R("geng -d1D9 12 16:19", 'D', 4).empty())
+        return false;
+    if(!check_R("geng -d1D8 11 15:17", 'D', 4).empty())
+        return false;
+    if(!check_R("geng -d1D3 10 13:13", 'D', 4).empty())
+        return false;
+
+
+
+    // SUR 8
+
+    // checks, whether D. wins (going second) on every graph on at most 8 
+    // vertices, minimum degree 1 and sum_{v != w} |deg(v)-3| <= 2, 
+    // where w is a maxdeg vtx.
+    for (int r = 2; r <= 8; ++r) {
+        if(!check_R("geng -d1 " + to_string(r), 'S', 2).empty())
+            return false;
+    }
+
+    
     cout << "ALL RSI-LEMMAS CHECKED SUCCESSFULLY" << endl;
     return true;
 }
@@ -84,7 +124,7 @@ bool check_SI(string cmd, char firstPlayer, int sur) {
     return true;
 }
 
-list<string> check_R(string cmd, int sur) {
+list<string> check_R(string cmd, char firstPlayer, int sur) {
     list<string> out;
     FILE *pipe = popen(cmd.c_str(), "r");
     if (!popen) {
@@ -98,6 +138,8 @@ list<string> check_R(string cmd, int sur) {
     int numOfDomWinGraphs = 0;
     while(fgets(buffer, sizeof(buffer), pipe)) {
         g6 = buffer;
+        if (g6.back() == '\n')
+            g6.pop_back();
         Graph g(g6);
         int maxDeg = 0;
         int maxDegVtx;
@@ -117,7 +159,7 @@ list<string> check_R(string cmd, int sur) {
         }
         else {
             GameState gs = GameState(&g);
-            if (gs.outcome('S') == 'D')
+            if (gs.outcome(firstPlayer) == 'D')
                 ++numOfDomWinGraphs;
             else {
                 cout << g6 << endl;
@@ -127,7 +169,11 @@ list<string> check_R(string cmd, int sur) {
     }
     pclose(pipe);
     cout << out.size() << " bad graphs, " << numOfGraphsWithWrongDegSeq << " graphs with surplus greater than ";
-    cout << sur << " and " << numOfDomWinGraphs << " graphs on which Dominator wins going second." << endl;
+    cout << sur << " and " << numOfDomWinGraphs << " graphs on which Dominator wins going ";
+    if (firstPlayer == 'D') 
+        cout << "first." << endl;
+    else if (firstPlayer == 'S')
+        cout << "second." << endl;
     
     return out;
 }
--- main.cpp
+++ main.cpp
@@ -11,31 +11,31 @@
 
 using namespace std;
 
-int main(int argc, const char *argv[]) {
-    string graph6_in;
-    int count=0;
-    clock_t c_start = clock();
-    while (getline(cin, graph6_in))
-    {
-        Graph graph(graph6_in);
-        Core core(&graph);
-        if (! core.completion_filter2())
-        {
-            cout << graph6_in;
-            cout << '\n';
-            count++;
-        }
-    }
-    clock_t c_end = clock();
-    double time = double(c_end - c_start) / CLOCKS_PER_SEC;
-    cerr.precision(2);
-    cerr << ">Z " << count << " graphs passed filter in " << time << " sec" << endl;
-    return 0;
-}
-
-// // check RSI Lemmas
-
-// int main() {
-//     check_all_RSI_Lemmas();
+// int main(int argc, const char *argv[]) {
+//     string graph6_in;
+//     int count=0;
+//     clock_t c_start = clock();
+//     while (getline(cin, graph6_in))
+//     {
+//         Graph graph(graph6_in);
+//         Core core(&graph);
+//         if (! core.completion_filter2())
+//         {
+//             cout << graph6_in;
+//             cout << '\n';
+//             count++;
+//         }
+//     }
+//     clock_t c_end = clock();
+//     double time = double(c_end - c_start) / CLOCKS_PER_SEC;
+//     cerr.precision(2);
+//     cerr << ">Z " << count << " graphs passed filter in " << time << " sec" << endl;
 //     return 0;
 // }
+
+// check RSI Lemmas
+
+int main() {
+    check_all_RSI_Lemmas();
+    return 0;
+}
