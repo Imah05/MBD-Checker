e9a8061e5690de23d0d00e75eb3710ef393a576d
Paul Hametner <hametnerpaul05@gmail.com>
2025-06-02 14:02:43 +0200

    SI Lemmas first try

--- RSI.cpp
+++ RSI.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// this checks Lemma with the number 2 in the table
+// this checks the Lemma with the number 2 in the R-table
 bool check_Lemma_R2() {
     cout << endl << endl<< "Checking Lemma #2 in the R-Lemma table..." << endl;
     if (check_R("geng -d1 5", 'S', 5).size() > 3)
@@ -13,7 +13,7 @@ bool check_Lemma_R2() {
     return true;
 }
 
-// this checks Lemma with the number 3 in the table
+// this checks the Lemma with the number 3 in the R-table
 bool check_Lemma_R3() {
     cout << endl << endl<< "Checking Lemma #3 in the R-Lemma table..." << endl;
     if(!check_R("geng -d1 4", 'S', 2).empty())
@@ -28,7 +28,7 @@ bool check_Lemma_R3() {
     return true;
 }
 
-// this checks Lemma with the number 4 in the table
+// this checks the Lemma with the number 4 in the R-table
 bool check_Lemma_R4() {
     cout << endl << endl<< "Checking Lemma #4 in the R-Lemma table..." << endl;
     if(!check_R("geng -d1D3 9 11:11", 'D', 5).empty())
@@ -43,7 +43,7 @@ bool check_Lemma_R4() {
     return true;
 }
 
-// this checks Lemma with the number 6 in the table
+// this checks the Lemma with the number 6 in the R-table
 bool check_Lemma_R6() {
     cout << endl << endl<< "Checking Lemma #6 in the R-Lemma table..." << endl;
     if (!check_R("geng -d2D9 13 17:20", 'D', 5).empty())
@@ -53,6 +53,91 @@ bool check_Lemma_R6() {
     cout << "Lemma #6 in the R-Lemma table checked successfuly" << endl << endl;
     return true;
 }
+
+// this checks the Lemma with the number 1 in the SI-table
+bool check_Lemma_SI1() {
+    cout << endl << endl<< "Checking Lemma #1 in the SI-Lemma table..." << endl;
+    if (check_SI("genbg -d2:3 -D9:3 7 9", 'D', 2))
+        return false;
+    if (check_SI("genbg -d2:3 -D8:3 6 8", 'D', 2))
+        return false;
+    cout << "Lemma #1 in the SI-Lemma table checked successfuly" << endl << endl;
+    return true;
+}
+
+// this checks the Lemma with the number 2 in the SI-table
+bool check_Lemma_SI2() {
+    cout << endl << endl<< "Checking Lemma #2 in the SI-Lemma table..." << endl;
+    if (!check_SI("genbg -d2:3 -D7:3 5 7", 'S', 2))
+        return false;
+    if (!check_SI("genbg -d2:3 -D6:3 4 6", 'D', 2))
+        return false;
+    if (!check_SI("genbg -d2:3 -D5:3 3 5", 'D', 2))
+        return false;
+    cout << "Lemma #2 in the SI-Lemma table checked successfuly" << endl << endl;
+    return true;
+}
+
+// this checks the Lemma with the number 3 in the SI-table
+bool check_Lemma_SI3() {
+    cout << endl << endl<< "Checking Lemma #3 in the SI-Lemma table..." << endl;
+    if (!check_SI("genbg -d2:3 -D4:3 9 10", 'D', 5))
+        return false;
+    cout << "Lemma #3 in the SI-Lemma table checked successfuly" << endl << endl;
+    return true;
+}
+
+// this checks the Lemma with the number 4 in the SI-table
+bool check_Lemma_SI4() {
+    cout << endl << endl<< "Checking Lemma #4 in the SI-Lemma table..." << endl;
+    if (!check_SI("genbg -d2:3 -D8:3 8 9", 'D', 5))
+        return false;
+    cout << "Lemma #4 in the SI-Lemma table checked successfuly" << endl << endl;
+    return true;
+}
+
+// this checks the Lemma with the number 5 in the SI-table
+bool check_Lemma_SI5() {
+    cout << endl << endl<< "Checking Lemma #5 in the SI-Lemma table..." << endl;
+    if (!check_SI("genbg -d2:3 -D8:3 7 8", 'D', 5))
+        return false;
+    cout << "Lemma #5 in the SI-Lemma table checked successfuly" << endl << endl;
+    return true;
+}
+
+// this checks the Lemma with the number 6 in the SI-table
+bool check_Lemma_SI6() {
+    cout << endl << endl<< "Checking Lemma #6 in the SI-Lemma table..." << endl;
+    if (!check_SI("genbg -d2:2 -D9:3 7 9 26:26", 'D', 4))
+        return false;
+    if (!check_SI("genbg -d2:2 -D8:3 6 8 23:23", 'D', 4))
+        return false;
+    if (!check_SI("genbg -d2:2 -D7:3 5 7 20:20", 'D', 4))
+        return false;
+    if (!check_SI("genbg -d2:2 -D6:3 4 6 17:17", 'D', 4))
+        return false;
+    if (!check_SI("genbg -d2:2 -D5:3 3 5 14:14", 'D', 4))
+        return false;
+    cout << "Lemma #6 in the SI-Lemma table checked successfuly" << endl << endl;
+    return true;
+}
+
+// this checks the Lemma with the number 7 in the SI-table
+bool check_Lemma_SI7() {
+    cout << endl << endl<< "Checking Lemma #7 in the SI-Lemma table..." << endl;
+    if (!check_SI("genbg -d2:3 -D7:3 6 7", 'S', 5))
+        return false;
+    if (!check_SI("genbg -d2:3 -D6:3 5 6", 'S', 5))
+        return false;
+    if (!check_SI("genbg -d2:3 -D5:3 4 5", 'S', 5))
+        return false;
+    if (!check_SI("genbg -d2:3 -D4:3 3 4", 'S', 5))
+        return false;
+    cout << "Lemma #7 in the SI-Lemma table checked successfuly" << endl << endl;
+    return true;
+}
+
+
 
 bool check_SI(string cmd, char firstPlayer, int sur) {
     FILE *pipe = popen(cmd.c_str(), "r");
