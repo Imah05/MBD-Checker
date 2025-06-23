88fe8c1d2060b5f73cc300b298934270367ee990
Imah05 <hametnerpaul05@gmail.com>
2025-06-23 09:32:36 +0200

    starting to add completion checker

---
+++ completion_checker.cpp
@@ -0,0 +1,33 @@
+#include "completion_checker.h"
+#include <cstring>
+
+// TODO
+
+// bool completionCheck(string cmd, vector<set<int>>) {
+//     FILE *pipe = popen(cmd.c_str(), "r");
+//     if (!popen) {
+//         pclose(pipe);
+//         throw runtime_error("completionCheck: popen failed");
+//     }
+//     char buffer[128];
+//     string graph6;
+
+//     while (fgets(buffer, sizeof(buffer), pipe)) {
+//         buffer[strcspn(buffer, "\n")] = 0;
+//         graph6 = buffer;
+
+//         PartComplCoreGameState pccgs(graph6);
+//         int numOfDeg2Vtx = 0;
+//         for(int i = 0; i < pccgs.getN(); i++) {
+//             if (pccgs.deg(i) == 2)
+//                 ++numOfDeg2Vtx;
+//         }
+//         if (numOfDeg2Vtx <= surBound && pccgs.outcome(firstPlayer) == 'S') {
+//             cout << "Exceptional graph found in checkSI: " << graph6 << endl;
+//             return false;
+//         }
+//     }
+//     pclose(pipe);
+//     return true;
+// }
+
---
+++ completion_checker.h
@@ -0,0 +1,9 @@
+#ifndef COMPLETION_CHECKER_H
+#define COMPLETION_CHECKER_H
+
+#include "part_compl_core_game_state.h"
+#include <list>
+
+// bool completionCheck(string cmd, vector<vector<int>>);
+
+#endif // COMPLETION_CHECKER_H
