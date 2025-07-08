2240432f1239d8cbf3d970350c2655c57e759d0d
fuejak01 <jfuehrer@strauss.algebra.uni-linz.ac.at>
2025-07-08 12:42:15 +0200

    Still trying to push my files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "part_compl_core_game_state.h"
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    checkFile(argv[1]);
+    checkFile(argv[1], atoi(argv[2]), atoi(argv[3]));
 }
