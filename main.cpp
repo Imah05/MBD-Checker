fda5840300ae122031b4e98a0ed510537a374cfe
fuejak01 <jfuehrer@strauss.algebra.uni-linz.ac.at>
2025-07-08 13:33:53 +0200

    added the main.cpp back again

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "part_compl_core_game_state.h"
+
+
+#include <iostream>
+
+
+#include <fstream>
+
+
+#include <stdlib.h>
+
+
+
+
+
+
+
+
+using namespace std;
+
+
+
+
+
+int main(int argc, char** argv) {
+
+
+    checkFile(argv[1], atoi(argv[2]), atoi(argv[3]));
+
+
+}
